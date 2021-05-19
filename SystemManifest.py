f35517ac5047091f2e79b2937ab87491620dc007
Jakob Breu <jakob.breu@daimler.com>
2021-05-19 21:50:08 +0200

    first version supporting google repo tool based

--- SystemManifest.py
+++ SystemManifest.py
@@ -1,6 +1,7 @@
 from Manifest import Manifest
 import config
 import Processor
+import os
 
 class SystemManifest(Manifest):
 
@@ -16,10 +17,15 @@ class SystemManifest(Manifest):
             repo_short = repo[repo.index("/")+1:]
             #print(s.attributes['name'].value)
 
-            if Path(f"{folder}/{repo_short}").is_dir():
-                Git(f"{folder}/{repo_short}").pull()
+            if repo_short == "apricot-manifests":
+                fldr = folder+"/"+repo_short
+                Path(fldr).mkdir(parents=True, exist_ok=True)
+                os.system(f"cd {fldr}; ./../../../repo init -u {config.baseurl}{repo}.git -b master; ./../../../repo sync --no-clone-bundle -c --optimized-fetch -j 10")
             else:
-                Path(folder).mkdir(parents=True, exist_ok=True)
-                Git(folder).clone(f"{config.baseurl}{repo}.git", depth=1)
+                if Path(f"{folder}/{repo_short}").is_dir():
+                    Git(f"{folder}/{repo_short}").pull()
+                else:
+                    Path(folder).mkdir(parents=True, exist_ok=True)
+                    Git(folder).clone(f"{config.baseurl}{repo}.git", depth=1)
 
             Processor.processNode(repo, folder, level+1)
