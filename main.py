cdf29f34bdff6a6e02000b8ba07b89807f52c130
jbreu <jakobbreu@gmail.com>
2021-09-05 10:58:07 +0200

    create tmp folder if it does not yet exist

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import Processor
 import config
 import TreePrinter
 
+import os
+
+if not os.path.exists('tmp'):
+    os.makedirs('tmp')
+
 root = Processor.processNode(config.systemrepo, "tmp/")
 
 TreePrinter.printTree(root)
